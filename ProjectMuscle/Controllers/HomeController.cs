b7b48e4ab37e731d827445791d5827701981cd5c
CalvinSithideth <calvin.sithideth@gmail.com>
2019-10-02 20:41:06 -0700

    Added Contact page to Home controller

--- ProjectMuscle/Controllers/HomeController.cs
+++ ProjectMuscle/Controllers/HomeController.cs
@@ -20,6 +20,12 @@ namespace ProjectMuscle.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
