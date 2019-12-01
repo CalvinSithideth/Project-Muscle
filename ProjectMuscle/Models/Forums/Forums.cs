e997f83ae6c11be043b3ab810448acbc3486df9e
CalvinSithideth <calvin.sithideth@gmail.com>
2019-11-30 16:59:06 -0800

    Added constructors to forum classes

--- ProjectMuscle/Models/Forums/Forums.cs
+++ ProjectMuscle/Models/Forums/Forums.cs
@@ -21,6 +21,17 @@ namespace ProjectMuscle.Models.Forums
         public string ForumDescription { get; set; }
 
         public List<ForumBoard> BoardList { get; set; }
+
+        /// <summary>
+        /// Creates a new forum.
+        /// </summary>
+        /// <param name="forumName"></param>
+        /// <param name="forumDescription"></param>
+        public Forum(string forumName, string forumDescription)
+        {
+            ForumName = forumName;
+            ForumDescription = forumDescription;
+        }
     }
 
     /// <summary>
@@ -38,6 +49,22 @@ namespace ProjectMuscle.Models.Forums
         public string BoardDescription { get; set; }
 
         public List<ForumThread> ThreadList { get; set; }
+
+        /// <summary>
+        /// Creates a new board.
+        /// </summary>
+        /// <param name="boardName">Name of the board. E.g. "Muay-Thai Board"</param>
+        /// <param name="boardDescription">Description of the board. E.g. "Southeast Asian Kickboxing"</param>
+        public ForumBoard(string boardName, string boardDescription)
+        {
+            BoardName = boardName;
+            BoardDescription = boardDescription;
+        }
+
+        public void AddThread(ForumThread newThread)
+        {
+            ThreadList.Add(newThread);
+        }
     }
 
     /// <summary>
@@ -53,6 +80,26 @@ namespace ProjectMuscle.Models.Forums
         public string ThreadTitle { get; set; }
 
         public List<ForumPost> PostList { get; set; }
+
+        /// <summary>
+        /// Creates a new thread from a thread title and a ForumPost.
+        /// </summary>
+        /// <param name="threadTitle">The title of the forum thread.</param>
+        /// <param name="InitialPost">The first post of the thread (original post).</param>
+        public ForumThread(string threadTitle, ForumPost InitialPost)
+        {
+            ThreadTitle = threadTitle;
+            PostList.Add(InitialPost);
+        }
+
+        /// <summary>
+        /// Adds a new post to the thread.
+        /// </summary>
+        /// <param name="newPost">The post to add to the thread.</param>
+        public void AddPost(ForumPost newPost)
+        {
+            PostList.Add(newPost);
+        }
     }
 
     /// <summary>
@@ -71,6 +118,18 @@ namespace ProjectMuscle.Models.Forums
         public DateTime PostDate { get; set; }
 
         public string PostContent { get; set; }
+
+        /// <summary>
+        /// Creates a new forum post.
+        /// </summary>
+        /// <param name="authorID">The ID of the user posting.</param>
+        /// <param name="postContent">The entire content string of the post.</param>
+        public ForumPost(int authorID, string postContent)
+        {
+            AuthorID = authorID;
+            PostDate = DateTime.Now;
+            PostContent = postContent;
+        }
 
     }
 }
