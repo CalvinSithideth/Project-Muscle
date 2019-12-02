c219c53b1e58fb66b6daecaa7579532b4e4dd217
CalvinSithideth <calvin.sithideth@gmail.com>
2019-12-01 19:12:22 -0800

    Added Forums Controller and default Views

---
+++ ProjectMuscle/Controllers/ForumsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjectMuscle.Data;
+using ProjectMuscle.Models.Forums;
+
+namespace ProjectMuscle.Controllers
+{
+    public class ForumsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ForumsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Forums
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Forum.ToListAsync());
+        }
+
+        // GET: Forums/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var forum = await _context.Forum
+                .FirstOrDefaultAsync(m => m.ForumID == id);
+            if (forum == null)
+            {
+                return NotFound();
+            }
+
+            return View(forum);
+        }
+
+        // GET: Forums/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Forums/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ForumID,ForumName,ForumDescription")] Forum forum)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(forum);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(forum);
+        }
+
+        // GET: Forums/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var forum = await _context.Forum.FindAsync(id);
+            if (forum == null)
+            {
+                return NotFound();
+            }
+            return View(forum);
+        }
+
+        // POST: Forums/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ForumID,ForumName,ForumDescription")] Forum forum)
+        {
+            if (id != forum.ForumID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(forum);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ForumExists(forum.ForumID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(forum);
+        }
+
+        // GET: Forums/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var forum = await _context.Forum
+                .FirstOrDefaultAsync(m => m.ForumID == id);
+            if (forum == null)
+            {
+                return NotFound();
+            }
+
+            return View(forum);
+        }
+
+        // POST: Forums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var forum = await _context.Forum.FindAsync(id);
+            _context.Forum.Remove(forum);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ForumExists(int id)
+        {
+            return _context.Forum.Any(e => e.ForumID == id);
+        }
+    }
+}
---
+++ ProjectMuscle/Data/Migrations/20191201012149_Forums1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectMuscle.Data.Migrations
+{
+    public partial class Forums1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Forum",
+                columns: table => new
+                {
+                    ForumID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ForumName = table.Column<string>(nullable: true),
+                    ForumDescription = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Forum", x => x.ForumID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ForumBoard",
+                columns: table => new
+                {
+                    BoardID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BoardName = table.Column<string>(nullable: true),
+                    BoardDescription = table.Column<string>(nullable: true),
+                    ForumID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ForumBoard", x => x.BoardID);
+                    table.ForeignKey(
+                        name: "FK_ForumBoard_Forum_ForumID",
+                        column: x => x.ForumID,
+                        principalTable: "Forum",
+                        principalColumn: "ForumID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ForumThread",
+                columns: table => new
+                {
+                    ThreadID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ThreadTitle = table.Column<string>(nullable: true),
+                    ForumBoardBoardID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ForumThread", x => x.ThreadID);
+                    table.ForeignKey(
+                        name: "FK_ForumThread_ForumBoard_ForumBoardBoardID",
+                        column: x => x.ForumBoardBoardID,
+                        principalTable: "ForumBoard",
+                        principalColumn: "BoardID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ForumPost",
+                columns: table => new
+                {
+                    PostID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AuthorID = table.Column<int>(nullable: false),
+                    PostDate = table.Column<DateTime>(nullable: false),
+                    PostContent = table.Column<string>(nullable: true),
+                    ForumThreadThreadID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ForumPost", x => x.PostID);
+                    table.ForeignKey(
+                        name: "FK_ForumPost_ForumThread_ForumThreadThreadID",
+                        column: x => x.ForumThreadThreadID,
+                        principalTable: "ForumThread",
+                        principalColumn: "ThreadID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ForumBoard_ForumID",
+                table: "ForumBoard",
+                column: "ForumID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ForumPost_ForumThreadThreadID",
+                table: "ForumPost",
+                column: "ForumThreadThreadID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ForumThread_ForumBoardBoardID",
+                table: "ForumThread",
+                column: "ForumBoardBoardID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ForumPost");
+
+            migrationBuilder.DropTable(
+                name: "ForumThread");
+
+            migrationBuilder.DropTable(
+                name: "ForumBoard");
+
+            migrationBuilder.DropTable(
+                name: "Forum");
+        }
+    }
+}
