3362e65f5fe288f1bae95b02eba93d43e54e1ee4
CalvinSithideth <calvin.sithideth@gmail.com>
2019-12-04 23:03:31 -0800

    Database Migration for Forums

---
+++ ProjectMuscle/Data/Migrations/20191203001507_Forums.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectMuscle.Data.Migrations
+{
+    public partial class Forums : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
